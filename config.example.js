674f796a3f0754bac6ac8a895d465ccbc4aae229
Cyril G <cyril@18ruedivona.eu>
2014-09-17 12:22:33 +0200

    Default configuration format according to configuration used by applications !

--- config.example.js
+++ config.example.js
@@ -1,18 +1,19 @@
-var
-  , tempo    = 'http://particuliers.edf.com/gestion-de-mon-contrat/options-tempo-et-ejp/option-tempo/la-couleur-du-jour-2585.html'
-  , ejp      = 'https://particuliers.edf.com/gestion-de-mon-contrat/options-tarifaires/option-ejp/l-observatoire-2584.html'
+var urlTempo = 'http://particuliers.edf.com/gestion-de-mon-contrat/options-tempo-et-ejp/option-tempo/la-couleur-du-jour-2585.html'
+  , urlEjp   = 'https://particuliers.edf.com/gestion-de-mon-contrat/options-tarifaires/option-ejp/l-observatoire-2584.html'
   , apiUrl   = ''
   , apiKey   = '';
 
 module.exports = {
   development: {
-    tempo: tempo,
-    ejp: ejp,
+    urlTempo: urlTempo,
+    urlEjp: urlEjp,
+    apiUrl: apiUrl,
     apiKey: apiKey
   },
   test: {
-    tempo: tempo,
-    ejp: ejp,
+    urlTempo: urlTempo,
+    urlEjp: urlEjp,
+    apiUrl: apiUrl,
     apiKey: apiKey
   },
   production: {}
