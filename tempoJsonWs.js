205e1c7e5a0488efea7229663149ea7d39c014f9
Cyril Gravelier <magiccyril@gmail.com>
2015-10-01 17:38:54 +0000

    Add tempo EDF JSON webservice

---
+++ tempoJsonWs.js
@@ -0,0 +1,69 @@
+var env = process.env.NODE_ENV || 'development'
+  , config = require('./config')[env]
+  , moment = require('moment')
+  , request = require('request');
+
+function parseColor (color) {
+  color = color.toLowerCase();
+
+  switch (color) {
+    case 'bleu':
+      return 'blue';
+
+    case 'blanc':
+      return 'white';
+
+    case 'rouge':
+      return 'red';
+    }
+};
+
+function createTempoObject (date, rawColor) {
+  var color = parseColor(rawColor);
+
+  if (!color) {
+    return null;
+  }
+  
+  return {
+    year: date.format('YYYY'),
+    month: date.format('MM'),
+    day: date.format('DD'),
+    color: color
+  };
+};
+
+function postToApi (tempo) {
+  request.post({
+    url: config.apiUrl + '/tempo?apikey=' + config.apiKey,
+    json: tempo
+  }, function(error, response, html) {
+    if (error || response.statusCode != 200) {
+      console.log('Error when saving');
+    }
+  });
+};
+
+function createWsUrl (base, type) {
+    var date = moment().format("YYYY-MM-DD");
+    return base + '?Date_a_remonter=' + date + '&TypeAlerte=' + type.toUpperCase();
+}
+
+request(createWsUrl(config.urlJsonTempo, 'tempo'), function (error, response, json) {
+  if (error) {
+    console.log('Cannot access EDF URL');
+    return;
+  }
+
+  var json = JSON.parse(json);
+  
+  var today = createTempoObject(moment(), json.JourJ.Tempo);
+  if (today) {
+    postToApi(today);
+  }
+
+  var tomorrow = createTempoObject(moment().add(1, 'day'), json.JourJ.Tempo);
+  if (tomorrow) {
+    postToApi(tomorrow);
+  }
+});
